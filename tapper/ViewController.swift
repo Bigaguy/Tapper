2df8748e1a0be98108cf1d04bc86c036a300d559
Andrew Ingram <andrewingram@Andrews-MacBook-Pro.local>
2016-02-03 19:51:36 -0500

    First Commit

--- tapper/ViewController.swift
+++ tapper/ViewController.swift
@@ -9,17 +9,55 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var tapsRequired:Int = 0
+    var currentTaps:Int = 0
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var tapsTextEdit: UITextField!
+    @IBOutlet weak var playBtn: UIButton!
+    @IBOutlet weak var tapper: UIImageView!
+    @IBOutlet weak var tapsLabel: UILabel!
+    @IBOutlet weak var tapBtn: UIButton!
+    
+
+    @IBAction func playBtn(sender: AnyObject) {
+        if tapsTextEdit.text != nil && tapsTextEdit != "" {
+        tapsRequired = Int(tapsTextEdit.text!)!
+        playBtn.enabled=false
+        playBtn.hidden=true
+        tapper.hidden=true
+        tapsTextEdit.hidden=true
+        tapBtn.hidden=false
+        tapsLabel.hidden=false
+        
+        }else{
+            tapsTextEdit.text="Please enter a value"
+        }
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func tapBtn(sender: AnyObject) {
+        currentTaps++
+        checkGameStatus()
+        updateLabel()
+        
+        
     }
+    
+    func checkGameStatus(){
+        if currentTaps==tapsRequired {
+            tapsLabel.text="You Won!"
+            tapBtn.enabled=false
+            
+        }
+    }
+    
+    func updateLabel(){
+        tapsLabel.text="\(currentTaps) Taps"
 
-
+    }
 }
+    
+
+
 
